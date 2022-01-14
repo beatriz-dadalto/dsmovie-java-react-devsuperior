e890e44294638e9da559173f7205a46d50b66f1c
Beatriz Coelho Dadalto <dadaltobeatriz@pm.me>
2022-01-14 11:47:54 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,16 +2,22 @@ import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
 import axios from 'axios';
 import { BASE_URL } from 'utils/requests';
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
 
 const Listing = () => {
+  const [pageNumber, setPageNumber] = useState(0);
 
-  // FORMA ERRADA
-  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
-    console.log(response.data);
-  });
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+    });
+  }, []);
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
